a8bc4ebd8a80232ae14f221f4cfcc77d65da9e73
sofyahrek <sofigrekal@gmail.com>
2023-07-19 14:30:16 +0300

    removed footer and result seceletons and styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import Container from "./Container";
 import Form from "./Form";
-import Footer from "./Footer";
 
 function App() {
   return (
     <Container>
       <Form />
-      <Footer/>
     </Container>
   );
 }
